e6f687ca84a86bb9cfd07b8c8266925e16025180
yumendy <yumendy@163.com>
2016-11-27 21:02:29 +0800

    使用百分比表示男女比例

--- statistics/mixin.py
+++ statistics/mixin.py
@@ -7,4 +7,10 @@ class InfoStateMixin(object):
         context['people_number'] = Info.objects.count()
         context['male_number'] = Info.objects.filter(sex='M').count()
         context['female_number'] = context['people_number'] - context['male_number']
+        if context['people_number'] != 0:
+            context['female_rate'] = "%.2f%%" % ((context['female_number'] * 1.0 / context['people_number']) * 100)
+            context['male_rate'] = "%.2f%%" % ((context['male_number'] * 1.0 / context['people_number']) * 100)
+        else:
+            context['female_rate'] = "%.2f%%" % 0
+            context['male_rate'] = "%.2f%%" % 0
         return context
